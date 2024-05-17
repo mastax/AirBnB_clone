424b6fa5a326189405bebc6d592aa220815ac333
mastax <elbahtouri.m@gmail.com>
2024-05-17 05:06:52 -0700

    the second task

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,19 @@ import models
 
 class BaseModel:
     """The BaseModel Class"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize attributes."""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
-
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+            if isinstance(kwargs['created_at'], str):
+                self.created_at = datetime.strptime(kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            if isinstance(kwargs['updated_at'], str):
+                self.updated_at = datetime.strptime(kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
     def __str__(self):
         """Retunr String Reprisentation."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
